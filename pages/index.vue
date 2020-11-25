9e086fbdd023e56a3d847213e56c6605331aeabe
Jason Low <72179247+jasonlowXD@users.noreply.github.com>
2020-11-25 21:29:57 +0800

    updated index

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,33 @@
 <template>
-  <div>
-    <div class='mt-20'>
-      <Logo style='width:120px;margin:auto' />
-      <h1 class='text-center text-3xl mt-10'>
-        NuxtJS KickStart Template
-      </h1>
-      <h2 class='text-center'>
-        Create simple, beautiful, and elegant website.
-      </h2>
-      
+  <div class="bg-grayish-white-1 relative">
+    <!-- display bg image -->
+    <div class="h-screen w-2/3">
+      <img
+        class="w-full h-full object-cover"
+        src="https://images.pexels.com/photos/331684/pexels-photo-331684.jpeg?cs=srgb&dl=pexels-math-331684.jpg&fm=jpg"
+      />
+    </div>
+    <!-- display name -->
+    <div class="absolute h-screen w-1/2 top-0 right-0">
+      <div class="flex h-full w-full">
+        <div class="my-auto w-full">
+          <div class="flex flex-col">
+            <div class="text-4xl font-poppins text-gray-800">
+              <span class="font-thin">welcome to</span>
+              <span class="font-bold">yeet page</span>
+              <span class="text-6xl text-lead-yellow">.</span>
+            </div>
+            <div class="font-thin text-gray-700 text-left -mt-4">
+              founder of yeet | yeet until niama also cannot recognize |
+              yeeeeeeeeeeet
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-}
+export default {};
 </script>
